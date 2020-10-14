a3966df94ec63674ae9056a5508640ac55473b77
Davi Gomes <davi.costa@aluno.faculdadeimpacta.com.br>
2020-10-14 01:01:34 -0300

    Configuracao inicial do CI

---
+++ prompt_setup.sh
@@ -0,0 +1,7 @@
+Script started on 2020-10-14 02:58:31+00:00 [TERM="xterm" TTY="/dev/pts/0" COLUMNS="65" LINES="29"]
+(venv) \u@\h:\w$  .  . ~/ .bashrd^[[D^[[D^[[D^[[D^[[D                    ^[[D    ^[[D    
+(venv) \u@\h:\w$ ls
+README.md  com	prompt_setup.sh  venv
+(venv) \u@\h:\w$ exit
+
+Script done on 2020-10-14 03:00:02+00:00 [COMMAND_EXIT_CODE="0"]
